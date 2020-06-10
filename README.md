8e8a202eee96e6c4be3c24e2b43d30ef92322efa
Mauricio Cartagena <mc973220@gmail.com>
2020-06-09 21:22:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flutter
+
+pagina Principal
---
+++ contador/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:contador/src/app.dart';
+
+//Nosotros escribimos
+// import './src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
