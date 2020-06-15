9d369cc914e1a5c2f2e75abd09a07ff3a1f59073
Mauricio Cartagena <mc973220@gmail.com>
2020-06-15 18:04:41 -0400

    ultimo estilo

--- disenos/lib/main.dart
+++ disenos/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
- 
+
 import 'package:disenos/src/pages/basico_page.dart';
 import 'package:disenos/src/pages/scroll_page.dart';
+import 'package:disenos/src/pages/botones_page.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.white
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
@@ -15,10 +21,11 @@ class MyApp extends StatelessWidget {
 
 
       //rutas
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
-        'basico' :(BuildContext context ) => BasicoPage(),
-        'scroll' :(BuildContext context ) => ScrollPage(),
+        'basico'  :(BuildContext context ) => BasicoPage(),
+        'scroll'  :(BuildContext context ) => ScrollPage(),
+        'botones' :(BuildContext context ) => BotonesPage(),
       },
     );
   }
